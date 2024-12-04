08571d7974c8c1dfd17c029da265867f18693932
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 08:55:53 -0600

    Update model-projects-by-employee.php

--- model-projects-by-employee.php
+++ model-projects-by-employee.php
@@ -2,7 +2,7 @@
 function selectProjectsbyEmployee($eid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("Select p.project_id, project_name, project_budget, start_date, end_date From project p join assignment a on a.project_id = p.project_id WHERE a.project_id=?");
+        $stmt = $conn->prepare("Select p.project_id, project_name, project_budget, start_date, end_date From project p join assignment a on a.project_id = p.project_id WHERE a.employee_id=?");
         $stmt->bind_param("i", $eid);
         $stmt->execute();
         $result = $stmt->get_result();
