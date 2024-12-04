b0163dfc565eb91e3015d6c64c279ddefc2c99cb
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 09:20:22 -0600

    Update assignments-by-project.php

--- assignments-by-project.php
+++ assignments-by-project.php
@@ -4,6 +4,6 @@ require_once("model-assignments-by-project.php");
 $pageTitle = "Assignments by Project";
 include "view-header.php";
 $assignments = selectAssignmentsByProject($_POST['pid']);
-include "view-assignments-by-projects.php";
+include "view-assignments-by-project.php";
 include "view-footer.php";
 ?>
