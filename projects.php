9048510c59b816ec70c87aebd0bc931b865bbf6d
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 03:22:01 -0600

    Update projects.php

--- projects.php
+++ projects.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-projects.php");
+$pageTitle = "Projects";
+include "view-header.php";
+$projects = selectProjects();
+include "view-projects.php";
+include "view-footer.php";
+?>
