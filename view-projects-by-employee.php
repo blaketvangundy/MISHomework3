d570df05e4f7d15bf1338fc13ed3b21dea0ebf6e
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 03:59:48 -0600

    Create view-projects-by-employee.php

---
+++ view-projects-by-employee.php
@@ -0,0 +1,29 @@
+<h1>Courses by Employee</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Budget</th>
+      <th>Start Date</th>
+      <th>End Date</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($project = $projects->fetch_assoc() {
+?>
+<tr>
+  <td><?php echo $project['project_id']; ?></td>
+  <td><?php echo $project['project_name']; ?></td>
+  <td><?php echo $project['project_budget']; ?></td>
+  <td><?php echo $project['start_date']; ?></td>
+  <td><?php echo $project['end_date']; ?></td>
+</tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
