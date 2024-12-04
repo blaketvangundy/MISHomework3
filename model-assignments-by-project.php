bc9517fb804891ec1d2cbd30aaf61ea4baf0acb1
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 09:07:58 -0600

    Update model-assignments-by-project.php

--- model-assignments-by-project.php
+++ model-assignments-by-project.php
@@ -3,7 +3,7 @@ function selectAssignmentsByProject($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("Select p.project_id, project_name, project_budget, start_date, end_date From project p join assignment a on a.project_id = p.project_id WHERE a.project_id=?");
-        $stmt->bind_param("i", $eid);
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
