9402b216f2f1493308a9bcfcfcf7f693da642f46
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 09:51:36 -0600

    Update view-employees-with-projects.php

--- view-employees-with-projects.php
+++ view-employees-with-projects.php
@@ -7,14 +7,16 @@ while ($employee = $employees->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
       <p class="card-text">
+    <ul class="list-group">
   <?php
    $projects = selectProjectsByEmployee($employee['employee_id']);
   while ($project = $projects->fetch_assoc()){
   ?>
-    
+      <li class="list-group-item"><?php echo $project['project_name']; ?> - <?php echo $project['project_budget']; ?> - <?php echo $project['start_date']; ?> - <?php echo $project['end_date']; ?></li>
   <?php
   }
   ?>
+    </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Position: <?php echo $employee['employee_position']; ?></p>
     </div>
