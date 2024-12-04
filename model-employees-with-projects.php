5521bdbafbcdb26105a9f554263cc35cddc95a61
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 09:36:16 -0600

    Update model-employees-with-projects.php

--- model-employees-with-projects.php
+++ model-employees-with-projects.php
@@ -12,4 +12,19 @@ function selectEmployees() {
         throw $e;
     }
 }
+
+function selectProjectsbyEmployee($eid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("Select p.project_id, project_name, project_budget, start_date, end_date From project p join assignment a on a.project_id = p.project_id WHERE a.employee_id=?");
+        $stmt->bind_param("i", $eid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
