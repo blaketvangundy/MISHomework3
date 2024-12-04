678dc6ca5ddb75ab710794eb5ed002e82cc62b7f
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-04 08:52:30 -0600

    Create assignments-by-project.php

---
+++ assignments-by-project.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php");
+require_once("model-assignments-by-project.php");
+$pageTitle = "Assignments by Project";
+include "view-header.php";
+$assignments = selectAssignmentsByProject($_POST['pid']);
+include "view-assignments-by-projects.php";
+include "view-footer.php";
+?>
