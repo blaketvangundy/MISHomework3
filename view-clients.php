884279b13e26923c4d6de30f46df83f08fc328bb
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-08 17:27:10 -0600

    Update view-clients.php

--- view-clients.php
+++ view-clients.php
@@ -1,1 +1,31 @@
-
+<h1>Clients</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Phone</th>
+      <th>Company Name</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($client = $clients->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $client['client_id']; ?></td>
+  <td><?php echo $client['client_name']; ?></td>
+  <td><?php echo $client['client_email']; ?></td>
+  <td><?php echo $client['client_phone']; ?></td>
+  <td><?php echo $client['company_name']; ?></td>
+  
+</tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
