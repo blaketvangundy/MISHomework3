b9304172ac76083e51ac87e709cc20ffbbfb829f
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-08 17:11:44 -0600

    Update client.php

--- client.php
+++ client.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-client.php");
+$pageTitle = "Clients";
+include "view-header.php";
+$employees = selectClients();
+include "view-clients.php";
+include "view-footer.php";
+?>
