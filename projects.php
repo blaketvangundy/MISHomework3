18f6c8110e7960c6cd905659302b35e4794b09df
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-10 10:52:07 -0600

    Update projects.php

--- projects.php
+++ projects.php
@@ -1,8 +1,17 @@
 <?php
 require_once("util-db.php");
 require_once("model-projects.php");
+
 $pageTitle = "Projects";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch($_PSOT['actionType'}){
+    case "Add":
+    insertProject($_POST['pName'], $_POST['pBudget'], $_POST['pLocation']);
+    break;
+  }
+}
 $projects = selectProjects();
 include "view-projects.php";
 include "view-footer.php";
