e758f2095d033de79e6aab1ccfd20e5947ab2ffe
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 15:11:08 -0700

    Update projects.php

--- projects.php
+++ projects.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch($_POST['actionType']){
     case "Add":
-    if (insertProjects($_POST['pName'], $_POST['pBudget'], $_POST['pLocation'])){
+    if (insertProjects($_POST['pName'], $_POST['pBudget'], $_POST['pLocation'],  $_POST['eid'])){
       echo '<div class="alert alert-success" role="alert"> Project Added.</div>';}
     else{
       echo '<div class="alert alert-danger" role="alert">Error</div>';
     }
     break;
         case "Edit":
-    if (updateProjects($_POST['pName'], $_POST['pBudget'], $_POST['pLocation'], $_POST['pid'])){
+    if (updateProjects($_POST['pName'], $_POST['pBudget'], $_POST['pLocation'], $_POST['eid'] $_POST['pid'])){
       echo '<div class="alert alert-success" role="alert"> Project Edited.</div>';}
     else{
       echo '<div class="alert alert-danger" role="alert">Error</div>';
