a85ff8fefb2a99919c233208a047b424fc4b3f37
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 16:31:04 -0700

    Update view-employee-input-list.php

--- view-employee-input-list.php
+++ view-employee-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="eid" name = "eid">
 <?php
 while ($employeeItem = $employeeList->fetch_assoc()){
+  $seltext = "";
+  if($selectedEmployee == $employeeItem['employee_id']){
+    $seltext = "selected";
+  }
 ?>
-  <option value="<?php echo $employeeItem['employee_id']?>"><?php echo $employeeItem['employee_name']?></option>
+  <option value="<?php echo $employeeItem['employee_id']?>"<?=$seltext?>><?php echo $employeeItem['employee_name']?></option>
   <?php
 }
 ?>
