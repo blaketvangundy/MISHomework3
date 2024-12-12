50ca25716e613ac4fa343ba503937424b18b45ae
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 12:09:49 -0700

    Update assignments-by-employee.php

--- assignments-by-employee.php
+++ assignments-by-employee.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-assignments-by-employee.php");
 $pageTitle = "Assignments by Employee";
 include "view-header.php";
-$assignments = selectAssignmentsByEmployee($_POST['pid']);
+$assignments = selectAssignmentsByEmployee($_POST['eid']);
 include "view-assignments-by-employee.php";
 include "view-footer.php";
 ?>
