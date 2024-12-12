db4ce9b893a3c4a634ab2f0082af6a931011594a
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 12:10:37 -0700

    Create view-assignments-by-employee.php

---
+++ view-assignments-by-employee.php
@@ -0,0 +1,27 @@
+<h1>Assignments by Employee</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Role</th>
+      <th>Start Date</th>
+      <th>End Date</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($assignment = $assignments->fetch_assoc()) {
+?>
+<tr>
+  <td><?php echo $assignment['assignment_id']; ?></td>
+  <td><?php echo $assignment['assignment_role']; ?></td>
+  <td><?php echo $assignment['start_date']; ?></td>
+  <td><?php echo $assignment['end_date']; ?></td>
+</tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
