0ed3b9d5197aadb31929ef48120afdfe417a0724
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 17:48:47 -0700

    Update view-employees-with-projects.php

--- view-employees-with-projects.php
+++ view-employees-with-projects.php
@@ -1,27 +1,29 @@
-<h1>Employees with Assignments</h1>
-<div class="card-group">
-<?php
-while ($employee = $employees->fetch_assoc()) {
-?>
-    <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
-      <p class="card-text">
-    <ul class="list-group">
-  <?php
-   $projects = selectProjectsByEmployee($employee['employee_id']);
-  while ($project = $projects->fetch_assoc()){
-  ?>
-      <li class="list-group-item"><?php echo $project['project_name']; ?> - <?php echo $project['project_budget']; ?> - <?php echo $project['start_date']; ?> - <?php echo $project['end_date']; ?></li>
-  <?php
-  }
-  ?>
-    </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Position: <?php echo $employee['employee_position']; ?></p>
-    </div>
+<div class="container bg-light p-4">
+  <h1>Employees with Assignments</h1>
+  <div class="card-group">
+    <?php
+    while ($employee = $employees->fetch_assoc()) {
+    ?>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $employee['employee_name']; ?></h5>
+          <p class="card-text">
+            <ul class="list-group">
+              <?php
+              $projects = selectProjectsByEmployee($employee['employee_id']);
+              while ($project = $projects->fetch_assoc()) {
+              ?>
+                <li class="list-group-item"><?php echo $project['project_name']; ?> - <?php echo $project['project_budget']; ?> - <?php echo $project['start_date']; ?> - <?php echo $project['end_date']; ?></li>
+              <?php
+              }
+              ?>
+            </ul>
+          </p>
+          <p class="card-text"><small class="text-body-secondary">Position: <?php echo $employee['employee_position']; ?></small></p>
+        </div>
+      </div>
+    <?php
+    }
+    ?>
   </div>
-<?php
-}
-?>
 </div>
