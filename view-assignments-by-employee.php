b7d236f21033421298ed191a36a99c8748abc404
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 17:47:06 -0700

    Update view-assignments-by-employee.php

--- view-assignments-by-employee.php
+++ view-assignments-by-employee.php
@@ -1,27 +1,24 @@
-<h1>Assignments by Employee</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-      <th>ID</th>
-      <th>Role</th>
-      <th>Start Date</th>
-      <th>End Date</th>
-      </tr>
-    </thead>
-    <tbody>
-<?php
-while ($assignment = $assignments->fetch_assoc()) {
-?>
-<tr>
-  <td><?php echo $assignment['assignment_id']; ?></td>
-  <td><?php echo $assignment['assignment_role']; ?></td>
-  <td><?php echo $assignment['start_date']; ?></td>
-  <td><?php echo $assignment['end_date']; ?></td>
-</tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
-</div>
+<div class="container bg-light p-4">
+  <h1>Assignments by Employee</h1>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Role</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($assignment = $assignments->fetch_assoc()) {
+        ?>
+          <tr>
+            <td><?php echo $assignment['assignment_id']; ?></td>
+            <td><?php echo $assignment['assignment_role']; ?></td>
+            <td><?php echo $assignment['start_date']; ?></td>
+            <td><?php echo $assignment['end_date']; ?></td>
+          </tr>
+        <?php
+      
