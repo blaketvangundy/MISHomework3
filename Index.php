537c01f70bc95039469f6d871203ceab2eba054a
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 17:20:40 -0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,7 +2,12 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework 4</h1>
+
+<h1>Homework 4</h1>
+
+<!-- Add the image -->
+<img src="https://www.hashmicro.com/blog/wp-content/uploads/2022/09/1-1.jpg" alt="Homework 4 Image" class="img-fluid" />
+
 <?php
 include "view-footer.php";
 ?>
