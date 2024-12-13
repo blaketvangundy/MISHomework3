1507d31c1c86897bc7bd08913bc9c81a6534c5b7
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-12-12 17:38:36 -0700

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,32 +8,33 @@
   </head>
   <body>
     <div class ="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="employees.php">Employees</a>
-        </li>
-                <li class="nav-item">
-          <a class="nav-link" href="projects.php">Projects</a>
-        </li>
-                <li class="nav-item">
-          <a class="nav-link" href="employees-with-projects.php">Employees with Assignments and More</a>
-        </li>
-                        <li class="nav-item">
-          <a class="nav-link" href="client.php">Clients</a>
-                                    <li class="nav-item">
-          <a class="nav-link" href="map.php">Map of Projects</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="employees.php">Employees</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="projects.php">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="employees-with-projects.php">Employees with Assignments and More</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="client.php">Clients</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="map.php">Map of Projects</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
